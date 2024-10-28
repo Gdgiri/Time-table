29c8e66332230ded02658112c395fb38a3f6568b
giridharan <giridharan831@gmail.com>
2024-10-28 13:23:23 +0530

    update table some changes in schedule

--- src/Pages/Timetable.jsx
+++ src/Pages/Timetable.jsx
@@ -4,19 +4,37 @@ import { Table } from "flowbite-react";
 
 const timetableData = [
   { time: "8:00 AM", activity: "Wake Up" },
-  { time: "8:00 - 8:30 AM", activity: "Morning Routine (Coffee, Food, Bath, Pray)" },
-  { time: "8:30 - 9:00 AM", activity: "Study Session 1 (Focus on theory/concepts)" },
-  { time: "9:00 - 10:00 AM", activity: "Study Session 2 (Problem Solving/Practice)" },
+  {
+    time: "8:00 - 8:30 AM",
+    activity: "Morning Routine (Coffee, Food, Bath, Pray)",
+  },
+  {
+    time: "8:30 - 9:00 AM",
+    activity: "Study Session 1 (Focus on which you are in Weak concepts)",
+  },
+  {
+    time: "9:00 - 10:00 AM",
+    activity: "Study Session 2 (Problem Solving/Practice)",
+  },
   { time: "10:00 AM - 1:00 PM", activity: "Online Class" },
   { time: "1:00 - 1:30 PM", activity: "Lunch Break" },
-  { time: "1:30 - 2:30 PM", activity: "Project Work (Hands-on development)" },
+  {
+    time: "1:30 - 2:30 PM",
+    activity: "Project Work (Learn new like firebasr,socket.io..etc)",
+  },
   { time: "2:30 - 3:00 PM", activity: "Break/Rest" },
-  { time: "3:00 - 4:00 PM", activity: "Study Session 3 (Learning new topics)" },
+  { time: "3:00 - 4:00 PM", activity: "Study Session 3 (Interview MERN)" },
   { time: "4:00 - 4:30 PM", activity: "Workout" },
   { time: "4:30 - 5:00 PM", activity: "Afternoon Break (Coffee, Relax)" },
-  { time: "5:00 - 6:30 PM", activity: "Study Session 4 (Problem Solving Practice)" },
+  {
+    time: "5:00 - 6:30 PM",
+    activity: "Study Session 4 (Problem Solving Practice)",
+  },
   { time: "6:30 - 7:00 PM", activity: "Dinner/Evening Meal" },
-  { time: "7:00 - 9:00 PM", activity: "Project Work (Focus on finalizing tasks)" },
+  {
+    time: "7:00 - 9:00 PM",
+    activity: "Project Work (Focus on finalizing tasks)",
+  },
   { time: "9:00 - 9:30 PM", activity: "Break (Relax, Refresh)" },
   { time: "9:30 - 10:30 PM", activity: "Wind Down (Prepare for sleep)" },
   { time: "11:00 PM", activity: "Sleep" },
@@ -24,12 +42,9 @@ const timetableData = [
 
 console.log(timetableData);
 
-
 console.log(timetableData);
 
-
 const Timetable = () => {
-    
   return (
     <div className="container mx-auto mt-5 p-4">
       <h2 className="text-4xl font-semibold mb-4 text-pinker text-center">
